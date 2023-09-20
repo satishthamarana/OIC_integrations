7500d9168858499f1f4474e918a88cc5276b857f
satishthamarana <satish.thamarana@gmail.com>
2023-09-20 08:36:20 +0000

    new changes in OIC

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ OIC_integrations
 
 
 CSF_IMPSUPADD_CALLBACK_RECIPE%7C01.00.0000
+
+$ python oic-cd.py 99214919-ora016 Welcome@2023 https://oic99214919-ocuocictrng29-ld.integration.ocp.oraclecloud.com CSF_IMPSUPADD_CALLBACK_RECIPE 01.00.0000
--- oic-cd.py
+++ oic-cd.py
@@ -9,16 +9,21 @@ URL = sys.argv[3]
 APP_VAR = sys.argv[4]
 APP_VAR_VER = sys.argv[5]
 
-# Export Integration Step
-print("This is an Export Integration step")
-os.system(f'curl -vvv -X GET -u {OIC_USERNAME}:{OIC_PASSWORD} -o "myfile_01.iar" {URL}/ic/api/integration/v1/integrations/{APP_VAR}%7C{APP_VAR_VER}/archive')
-time.sleep(30)
+try:
+    # Export Integration Step
+    print("This is an Export Integration step")
+    os.system(f'curl -v -X GET -u {OIC_USERNAME}:{OIC_PASSWORD} -o "myfile_01.iar" {URL}/ic/api/integration/v1/integrations/{APP_VAR}%7C{APP_VAR_VER}/archive')
+    time.sleep(30)
 
-# Import Integration Step
-print("This is an Import Integration step")
-os.system(f'curl -vvv -s -u {OIC_USERNAME}:{OIC_PASSWORD} -H "Accept: application/json" -X PUT -F "file=@myfile_01.iar;type=application/octet-stream" {URL}/ic/api/integration/v1/integrations/archive')
+    # Import Integration Step
+    print("This is an Import Integration step")
+    os.system(f'curl -s -u {OIC_USERNAME}:{OIC_PASSWORD} -H "Accept: application/json" -X PUT -F "file=@myfile_01.iar;type=application/octet-stream" {URL}/ic/api/integration/v1/integrations/archive')
 
-# Activate Integration Step
-# print("This is an Activate Integration step")
-# os.chdir("oic-migration")
-# os.system(f'curl -X POST -u {OIC_USERNAME}:{OIC_PASSWORD} -H "Content-Type:application/json" -H "X-HTTP-Method-Override:PATCH" -d @activate.json {URL}/ic/api/integration/v1/integrations/{APP_VAR}')
+    # Activate Integration Step
+    print("This is an Activate Integration step")
+    os.system(f'curl -v -X POST -u {OIC_USERNAME}:{OIC_PASSWORD} -H "Content-Type:application/json" -H "X-HTTP-Method-Override:PATCH" -d @activate.json {URL}/ic/api/integration/v1/integrations/{APP_VAR}%7C{APP_VAR_VER}')
+except Exception as error:
+    print("This is teh error occured: ", error) 
+finally:    
+    print("closing the connections")
+    os.system(f'curl -u {OIC_USERNAME}:{OIC_PASSWORD} -H "Connection: close" {URL}/ic/home')
