6ecc200a95cfa75aeddf4bd528331624e9e58570
satishthamarana <satish.thamarana@gmail.com>
2023-09-20 06:36:29 +0000

    new changes in OIC

--- oic-cd.py
+++ oic-cd.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 # Define environment variables
 OIC_USERNAME = os.environ.get("OIC_USERNAME")
@@ -9,10 +10,11 @@ APP_VAR = os.environ.get("APP_VAR")
 # Export Integration Step
 print("This is an Export Integration step")
 os.system(f'curl -vvv -X GET -u {OIC_USERNAME}:{OIC_PASSWORD} -o "myfile_01.iar" {URL}/ic/api/integration/v1/integrations/{APP_VAR}/archive')
+time.sleep(30)
 
 # Import Integration Step
-# print("This is an Import Integration step")
-# os.system(f'curl -vvv -s -u {OIC_USERNAME}:{OIC_PASSWORD} -H "Accept: application/json" -X PUT -F "file=@myfile_01.iar;type=application/octet-stream" {URL}/ic/api/integration/v1/integrations/archive')
+print("This is an Import Integration step")
+os.system(f'curl -vvv -s -u {OIC_USERNAME}:{OIC_PASSWORD} -H "Accept: application/json" -X PUT -F "file=@myfile_01.iar;type=application/octet-stream" {URL}/ic/api/integration/v1/integrations/archive')
 
 # Activate Integration Step
 # print("This is an Activate Integration step")
